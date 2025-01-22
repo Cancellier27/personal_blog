742c101a1662f7e57a7a90772431bd324b1738b9
Cancellier27 <filipecancelliercosta@gmail.com>
2025-01-22 17:45:53 +0000

    aside bar and main section divided and coloured

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,20 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-      <nav id="nav-tab-container" >nav tab</nav>
-      <div id="main-section-outermost-container" >
-        <section id="main-section-container" >Main section</section>
-        <footer id="footer-tag" >Footer</footer>
+      <aside id="nav-tab-container">
+        <h1 className="main-title">FCC</h1>
+        <nav className="navigation-tag">
+          <ul>
+            <li className="nav-item">Home</li>
+            <li className="nav-item">Search</li>
+            <li className="nav-item">News</li>
+          </ul>
+        </nav>
+        <footer>All right reserved </footer>
+      </aside>
+      <div id="main-section-outermost-container">
+        <section id="main-section-container">Main section</section>
+        <footer id="footer-tag">Footer</footer>
       </div>
     </div>
   )
