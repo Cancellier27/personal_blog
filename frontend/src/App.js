425eef870cf48537cf5867609286debfa1db60ce
Cancellier27 <filipecancelliercosta@gmail.com>
2025-01-24 19:04:46 +0000

    aside bar style upgraded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,34 @@
 import "./App.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faF } from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {
+  faHouse,
+  faMagnifyingGlass,
+  faPaperPlane
+} from "@fortawesome/free-solid-svg-icons"
 
 function App() {
   return (
     <div className="App">
       <aside id="aside-tab-container">
-        <h1 className="main-title title-font"> FCC </h1>
-        <nav className="navigation-tag">
-          <ul className="nav-items-container">
-            <li className="nav-item">Home</li>
-            <li className="nav-item">Search</li>
-            <li className="nav-item">News</li>
-          </ul>
-        </nav>
-        <footer>All right reserved </footer>
+        <div>
+          <h1 className="main-title title-font"> FCC </h1>
+          <nav className="navigation-tag">
+            <ul className="nav-items-container">
+              <a className="nav-item">
+                <FontAwesomeIcon icon={faHouse} /> <p>Home</p>
+              </a>
+              <li className="nav-item">
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                <p>Search</p>
+              </li>
+              <a className="nav-item">
+                <FontAwesomeIcon icon={faPaperPlane} />
+                <p>News</p>
+              </a>
+            </ul>
+          </nav>
+        </div>
+        <footer>All rights reserved &#169; </footer>
       </aside>
       <div id="main-section-outermost-container">
         <section id="main-section-container">Main section</section>
