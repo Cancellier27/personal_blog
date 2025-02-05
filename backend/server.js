a8d78d97ee40e04294c35161e12e9a3a570d75af
Cancellier27 <filipecancelliercosta@gmail.com>
2025-02-05 16:49:53 +0000

    created new get and post routes for the login page

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,24 @@
 const express = require("express")
 const cors = require("cors")
+const fs = require("fs")
+const db = require("./DB/db.json")
 const app = express()
 const PORT = process.env.PORT || 8000
-
-let UsingName = "FILIPE"
-
 
 app.use(express.json())
 app.use(cors())
 
-app.get("/home", (req, res) => {
-  res.json({message: UsingName})
+// to get the users list
+app.get("/login", (req, res) => {
+  res.json(db.users)
 })
 
-app.post("/name", (req, res) => {
-  
-  const { name } = req.body
-  
-  UsingName = name
-
-  res.status(201).json(`Name updated to ${name}`)
-  
+// post to change the json db to isLogged 
+app.post("/login/success", (req, res) => {
+  const {user, loginStatus} = req.body
+  db.users[user].isLogged = loginStatus
+  fs.writeFileSync("./DB/db.json", JSON.stringify(db, null, 2))
+  res.status(201).json(`${user} success login!`)
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
