948e431a11dac31811375f0d71fe4717ed315263
Cancellier27 <filipecancelliercosta@gmail.com>
2025-03-20 18:36:41 +0000

    fixed loading page hook async

--- frontend/src/components/news_page/index.jsx
+++ frontend/src/components/news_page/index.jsx
@@ -6,51 +6,56 @@ import {useEffect, useState} from "react"
 import axios from "axios"
 
 export default function NewsPage() {
-  const [newsData, setNewsData] = useState("")
+  const [newsData, setNewsData] = useState(false)
   const params = useParams()
 
   useEffect(() => {
-    try {
-      // fetch information from the news database
-      axios.get("http://localhost:8000/newsInformation").then((res) => {
-        Object.keys(res.data).forEach((key) => {
-          let formattedTitle = res.data[key].card.title.split(" ").join("-")
-          
-          // Check if the data matches the params passes in the URL
-          if (formattedTitle === params.newsId) {
-            setNewsData(res.data[key].news)
-          }
+    const getNews = async () => {
+      try {
+        // fetch information from the news database
+        await axios.get("http://localhost:8000/newsInformation").then((res) => {
+          Object.keys(res.data).forEach((key) => {
+            let formattedTitle = res.data[key].card.title.split(" ").join("-")
+
+            // Check if the data matches the params passed in the URL
+            if (formattedTitle === params.newsId) {
+              setNewsData(res.data[key])
+            }
+          })
         })
-      })
-    } catch (err) {
-      console.error(`An error happened fetching the news list`, err)
+      } catch (err) {
+        console.error(`An error happened fetching the news list`, err)
+      }
     }
+
+    // Calls the async function
+    getNews()
   }, [])
 
   return (
     <div className="App">
       <NavBar />
-      <div id="main-section-outermost-container">
-        <header className="news-selection-header">
-          <h1 className="news-selection-title news-title">
-            {newsData.title}
-          </h1>
-          <div className="news-selection-author-data">
-            <p>BY FILIPE COSTA</p>
-            <p>POSTED: JAN 26 2025</p>
-          </div>
-        </header>
-        <main className="news-selection-text-container">
-          <img src={One_Piece} alt="One piece logo image" />
-          <p>
-            {newsData.description}
-          </p>
-        </main>
-        <footer className="news-selection-footer-container">
-          <h1 className="title-font">FCC</h1>
-          <p>All rights reserved &#169;</p>
-        </footer>
-      </div>
+      {newsData && (
+        <div id="main-section-outermost-container">
+          <header className="news-selection-header">
+            <h1 className="news-selection-title news-title">
+              {newsData.news.title}
+            </h1>
+            <div className="news-selection-author-data">
+              <p>{`BY ${newsData.author}`}</p>
+              <p>{`POSTED: ${newsData.publishDate}`}</p>
+            </div>
+          </header>
+          <main className="news-selection-text-container">
+            <img src={One_Piece} alt={newsData.news.alt} />
+            <p>{newsData.news.description}</p>
+          </main>
+          <footer className="news-selection-footer-container">
+            <h1 className="title-font">FCC</h1>
+            <p>All rights reserved &#169;</p>
+          </footer>
+        </div>
+      )}
     </div>
   )
 }
